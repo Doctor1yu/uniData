fe080942fcbc73de8d57c61c81f0d59fb2055d8f
wanfeng <2460972805@qq.com>
2025-03-12 23:14:32 +0800

    初始化

---
+++ src/main/java/com/back/vuedata/VueDataApplication.java
@@ -0,0 +1,13 @@
+package com.back.vuedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/vuedata/VueDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.vuedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
